19f94583ccd6de81f5362575cc508ac118fded88
Caio <c.rombaldo@gestaoagropecuaria.com.br>
2023-03-13 11:14:57 -0300

    Adição da informação do bloco

--- script_cesu_labs.py
+++ script_cesu_labs.py
@@ -34,8 +34,15 @@ for lab in labs:
 
         # Filtra por apenas aulas de Engenharia de Software.
         if 'ESOFT5S-N-A'.upper() in reserva_name.upper():
+            
+            # Recupera a tabela pai que contém os elementos
+            table = lab.find_parent(name='table', class_='bloco')
+            # Procura na tabela pelo primeiro elemento tr (Cabeçalho)
+            bloco = table.find(name='tr').text
+
             aula = {
                 'Horário': horario,
+                'Bloco': bloco,
                 'Laboratório': lab_name,
                 'Reserva': reserva_name,
                 'Data_ETL': data_atual,
@@ -46,6 +53,7 @@ if not aulas_hoje:
     aulas_hoje.append(
         {
             'Horário': None,
+            'Bloco': None,
             'Laboratório': None,
             'Reserva': 'Nenhuma aula encontrada nos laboratórios',
             'Data_ETL': data_atual,
@@ -55,5 +63,5 @@ if not aulas_hoje:
 # Carregamento dos dados tratados.
 with open('aulas.json', 'w', encoding='utf-8') as arquivo_json:
     json.dump(
-        aulas_hoje, arquivo_json, ensure_ascii=False, indent=4, sort_keys=True
+        aulas_hoje, arquivo_json, ensure_ascii=False, indent=4, sort_keys=False
     )
